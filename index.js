bd33bd1672c26d61ad5f45c41564cc41fcc322a6
Artem-Stryzhakov <114780369+Artem-Stryzhakov@users.noreply.github.com>
2022-11-10 12:40:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const app = require('express')();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swapper.json')
+// const swaggerDocument = require('./docs/swapper.json')
+const mongodb = require('mongodb').MongoClient;
+const yamljs = require('yamljs');
+const swapperDocument = yamljs.load('./docs/swapper.yaml')
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+//const url = "mongodb+srv://artem:WebDevelop@fullstackproject.gx3iiu1.mongodb.net/?retryWrites=true&w=majority"
+
+/*app.post("/addData", (req, res) => {
+    mongodb.connect(url, (error, db) => {
+        if (error) throw error
+        const dbo = db.db('games-API');
+        dbo.collection("games").insertOne({name: "Witcher 3", price: 29.99}, (err) => {
+            if (error) throw err;
+            db.close();
+        })
+    })
+})*/
+
+const games = [
+    {id: 1, name: "Witcher 3", price: 29.99},
+    {id: 2, name: "For Honor", price: 29.99},
+    {id: 3, name: "Metro EXODUS", price: 19.99},
+    {id: 4, name: "Minecraft", price: 19.99},
+    {id: 5, name: "CS:Source", price: 5.99},
+    {id: 6, name: "The Elder Scrolls Online", price: 49.99},
+    {id: 7, name: "GTA 5", price: 29.99},
+    {id: 8, name: "Marvel Spider-man", price: 59.99},
+    {id: 9, name: "Forza Horizon 5", price: 59.99}
+]
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swapperDocument))
 
 app.get('/games', (req, res) => {
     res.send(["Witcher 3", "For Honor", "Metro EXODUS"])
+})
+
+app.get('/games/:id', (req, res) => {
+    if (typeof games[req.params.id - 1] === "undefined"){
+        return res.status(404).send({
+            error: "Game not found"
+        })
+    }
+    res.send(games[req.params.id - 1])
 })
 
 app.listen(8080, (err) => {
